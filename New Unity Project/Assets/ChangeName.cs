801514f1501107427c215d69fd33f4bf91d5e4d1
Your Name <Matthewjdemeo@knights.ucf.edu>
2019-06-30 20:41:42 -0400

    First commit

---
+++ New Unity Project/Assets/ChangeName.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class ChangeName: MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+        TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+        textmeshPro.SetText("Hello World from Matthew DeMeo"); 
+        Debug.Log ("Key Pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+
+    }
+}
